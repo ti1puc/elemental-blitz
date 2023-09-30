9814a01d31667f5c1dea191ff33f0f32fb798e0e
lcscout <lcscoutinho@outlook.com>
2023-09-30 18:55:50 -0300

    Simplified collision scripts

--- Assets/Scripts/Collisions/EnemyCollision.cs
+++ Assets/Scripts/Collisions/EnemyCollision.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyCollision : CollisionBase
+public class EnemyCollision : MonoBehaviour
 {
-    #region Fields
-    //[Header("Settings")]
-    //[Header("References")]
-    //[Header("Debug")]
-    #endregion
-    
-    #region Properties
-    #endregion
-    
-    #region Unity Messages
-    #endregion
-    
-    #region Public Methods
-    #endregion
-    
-    #region Private Methods
-    #endregion
+	#region Fields
+	//[Header("Settings")]
+	//[Header("References")]
+	//[Header("Debug")]
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region Unity Messages
+	private void OnTriggerEnter(Collider other)
+	{
+		// tomar dano e tal
+	}
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
 }
--- Assets/Scripts/Collisions/PlayerCollision.cs
+++ Assets/Scripts/Collisions/PlayerCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollision : CollisionBase
+public class PlayerCollision : MonoBehaviour
 {
 	#region Fields
 	//[Header("Settings")]
@@ -14,16 +14,9 @@ public class PlayerCollision : CollisionBase
 	#endregion
 
 	#region Unity Messages
-	protected override void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter(Collider other)
 	{
-		base.OnTriggerEnter(other);
-
 		// tomar dano e tal
-	}
-
-	protected override void OnTriggerExit(Collider other)
-	{
-		base.OnTriggerExit(other);
 	}
 	#endregion
 
